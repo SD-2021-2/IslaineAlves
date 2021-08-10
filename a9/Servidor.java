44c79c6c6067cb1b59cd72ef03c8808d6f8be94f
Islaine Alves <45128957+islainealves@users.noreply.github.com>
2021-08-10 02:00:41 -0300

    Add files via upload

--- a9/Servidor.java
+++ a9/Servidor.java
@@ -23,29 +23,9 @@ public class Servidor {
                array = mensagem.split("@");
                int valor=Integer.parseInt(array[0]);
                int naipe=Integer.parseInt(array[1]);
-               String extenso = "";
-               if(valor==1) {
-            	   extenso = "as";
-               }else if(valor==11) {
-            	   extenso = "valete";
-               }else if(valor==12) {
-            	   extenso = "dama";
-               }else if(valor==13) {
-            	   extenso = "rei";
-               }else {
-            	   extenso=valor+"";
-               }
-               if(naipe==1) {
-            	   extenso += " de ouros";
-               }else if(naipe==2) {
-            	   extenso += " de paus";
-               }else if(naipe==3) {
-            	   extenso += " de copas";
-               }else if(naipe==4) {
-            	   extenso += " de espadas";
-               }
+               Carta carta = new Carta(valor,naipe);
                
-               outToClient.writeBytes(extenso+"\n");    
+               outToClient.writeBytes(carta.imprimir()+"\n");    
            } 
       
        } catch (IOException e) {
